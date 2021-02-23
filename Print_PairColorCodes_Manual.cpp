ef7fb1ce0c206645a92ffcf64832bd8b6795a0f8
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-02-24 01:03:43 +0530

    To print Color Code Manual

---
+++ Print_PairColorCodes_Manual.cpp
@@ -0,0 +1,16 @@
+#include "TeleComColorCoder.h"
+
+#define LAST_COLORCODE  25
+#define FIRST_COLORCODE 1
+
+void Print_PairColorCodes_Manual(void)
+{
+
+for(int PairColorCode=FIRST_COLORCODE; PairColorCode <= LAST_COLORCODE ; PairColorCode++)
+{
+
+	TeleComColorCoder::ColorPair tmpcolorPair_classobj = TeleComColorCoder::Extract_Color_From_PairNumber(PairColorCode);
+	std::cout << PairColorCode << "\t" << tmpcolorPair_classobj.PrintColorPairString() << std::endl;
+}
+
+}
