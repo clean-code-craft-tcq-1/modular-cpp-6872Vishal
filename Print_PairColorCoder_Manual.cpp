a0aff4f0d158fccc8ba976b4c87e8f24f2fcf879
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-02-25 19:09:57 +0530

    Update Print_PairColorCoder_Manual.cpp

--- Print_PairColorCoder_Manual.cpp
+++ Print_PairColorCoder_Manual.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "TeleComColorCoder.h"
-using namespace TeleComColorCoder
+using namespace TeleComColorCoder;
 
 #define LAST_COLORCODE  25
 #define FIRST_COLORCODE 1
@@ -11,7 +11,7 @@ void Print_PairColorCodes_Manual(void)
 for(int PairColorCode=FIRST_COLORCODE; PairColorCode <= LAST_COLORCODE ; PairColorCode++)
 {
 
-	TeleComColorCoder::ColorPair tmpcolorPair_classobj = TeleComColorCoder::Extract_Color_From_PairNumber(PairColorCode);
+	ColorPair tmpcolorPair_classobj = Extract_Color_From_PairNumber(PairColorCode);
 	std::cout << PairColorCode << "\t" << tmpcolorPair_classobj.DeliverColorPairString() << std::endl;
 }
 
